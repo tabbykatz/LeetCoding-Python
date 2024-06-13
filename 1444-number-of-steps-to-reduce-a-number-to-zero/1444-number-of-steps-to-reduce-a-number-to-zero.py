4d0549fad4182bb866adfd063f97ff38da2f93ba
Tabitha Katz <tabitha.katz@upstart.com>
2024-06-12 18:50:10 -0700

    Time: 40 ms (21.46%), Space: 16.7 MB (9.47%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
@@ -4,17 +4,14 @@ class Solution:
         def numModder(num, steps):
             print(num, steps)
             if num == 0:
-                print("num is 0!")
                 return steps + 0
             elif num % 2 == 0:
                 num = num // 2
                 steps += 1
-                print("num was even!")
                 return numModder(num, steps)
 
             else:
                 num = num - 1
-                print("num was odd")
                 steps += 1
                 return numModder(num, steps)
             
